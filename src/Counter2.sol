9b7a54655a03fa7a5fc1ccbfc8b0f66b3f40e950
emrekap <emrekap@hotmail.com>
2023-08-23 20:33:09 +0300

    aa

---
+++ src/Counter2.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.3;
+
+import "openzeppelin/utils/math/SafeMath.sol";
+
+contract Counter2 {
+    using SafeMath for uint256;
+
+    function addition(
+        uint256 a,
+        uint256 b
+    ) public payable returns (bool, uint256) {
+        return a.tryAdd(b);
+    }
+
+    function multiply(
+        uint256 a,
+        uint256 b
+    ) public payable returns (bool, uint256) {
+        return SafeMath.tryMul(a, b);
+    }
+}
